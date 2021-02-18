89eb63341e314009364b9885e6c1233683e952fb
Imen Krad <imenkrad@MacBook-Air-de-Imen-2.local>
2021-02-18 16:17:47 +0100

    Ajout fichier python 22

---
+++ code.py
@@ -0,0 +1,28 @@
+words = ["Je", "tu", "il", "nous", "vous", "ils"]
+characters = ["imen", "Amine", "youssef", "myriam"]
+
+def try_valeur(test):
+  quote = test[4]
+  return quote 
+
+def method(word, character)
+  capitalize(word)
+  capitalize(character)
+  return "{} a dit: {}".format(word, character)
+  
+user_answer = ("Donner la valeur par defaut!")
+
+while user_answer != "B":
+  print(try_valeur(words))
+  user_answer = "B"
+
+for character in characters:
+  s_character = character.capitalize()	
+  print(s_character)
+
+for item in words:
+  n_item = item.capitalize()
+  print(n_item)
+
+
+print(try_valeur(words))
